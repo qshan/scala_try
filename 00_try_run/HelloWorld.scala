0d535510833361707de75afc42805175876c1af7
Frank Shan <frankqs0304@hotmail.com>
2022-06-03 23:05:33 +0800

    add print current function

--- 00_try_run/HelloWorld.scala
+++ 00_try_run/HelloWorld.scala
@@ -1,5 +1,8 @@
-object HelloWorld {
-    def main(args: Array[String]): Unit = {
-        println("Hello, world!")
-    }
+object HelloWorld
+{
+  def main(args: Array[String]): Unit =
+  {
+    println("Hello, world!")
+    printf ("Here is %s!\n" ,getClass.getName());
+  }
 }
