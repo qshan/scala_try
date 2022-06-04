3b0fbc10395edddd8c1890544f87a5ece3a51fed
Frank Shan <frankqs0304@hotmail.com>
2022-06-04 15:10:53 +0800

    update the format

--- 06_try_break/TryBreak.scala
+++ 06_try_break/TryBreak.scala
@@ -16,8 +16,7 @@ object TryBreak
     val tempTimes : Int  = 5;
 
     println ("---------- Try [break] here ----------");
-    //var tempCheckerBreakTryA : Int = 0;
-    var a : Int = 0;
+    var tempCheckerBreakTryA : Int = 0;
     val numListBreakTry = List(1,2,3,4,5,6,7,8,9,10);
 
     val loop = new Breaks;
