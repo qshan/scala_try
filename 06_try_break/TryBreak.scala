ebaa1b0c9db44f6ba03959de9ea519cfc4ef4135
Frank Shan <frankqs0304@hotmail.com>
2022-06-04 15:10:23 +0800

    update the format

--- 06_try_break/TryBreak.scala
+++ 06_try_break/TryBreak.scala
@@ -16,34 +16,25 @@ object TryBreak
     val tempTimes : Int  = 5;
 
     println ("---------- Try [break] here ----------");
-      //var tempCheckerBreakTryA : Int = 0;
-      var a : Int = 0;
-      val numList = List(1,2,3,4,5,6,7,8,9,10);
+    //var tempCheckerBreakTryA : Int = 0;
+    var a : Int = 0;
+    val numListBreakTry = List(1,2,3,4,5,6,7,8,9,10);
 
-      val loop = new Breaks;
-      loop.breakable {
-         for( tempCheckerBreakTryA <- numList){
-            println( "Get value of tempCheckerBreakTryA from numList: "
-                    + tempCheckerBreakTryA );
-            if( tempCheckerBreakTryA == 4 ){
-               loop.break;
-            }
-         }
+    val loop = new Breaks;
+    loop.breakable{
+      for( tempCheckerBreakTryA <- numListBreakTry)
+      {
+        println( "Get value of tempCheckerBreakTryA from numListBreakTry: "
+                + tempCheckerBreakTryA );
+        if( tempCheckerBreakTryA == 4 )
+        {
+          println( "Found value 4 in numListBreakTry: "
+                + tempCheckerBreakTryA );
+          loop.break;
+        }
       }
-      println( "After the loop" );
-
-
-
-
-    tempCounter = 0;
-    while (tempCounter < tempTimes)
-    {
-      printf ("current counter    is %d\n" ,tempCounter);
-      tempCounter +=  1;
-      tempValue =  tempCounter;
-      printf ("current tempValue  is %d\n" ,tempCounter);
     }
-    println ("--------------------");
+    println( "After the loop.breakable" );
 
     printf ("----------End of %s----------\n" ,getClass.getName());
     println ("--------------------");
