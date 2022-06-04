f0114e2c1b59ff8af4e81ba249c929d34fa74dbf
Frank Shan <frankqs0304@hotmail.com>
2022-06-05 00:11:43 +0800

    add func03 recursion function case

---
+++ 09_try_func03/TryFunc03.scala
@@ -0,0 +1,36 @@
+#!scala
+
+object TryFunc03
+{
+  def main(args: Array[String]): Unit =
+  {
+    println("Hello, world!");
+    //println("Here is the TryFunc03!");
+    //printf ("Here is %s!\n" ,"TryFunc03");
+    printf ("Here is %s!\n" ,getClass.getName());
+    println ("--------------------");
+    //printf ("--------------------\n");
+
+    println ("---------- Try [Recursion Functions] here ----------");
+
+    for (i <- 1 to 10)
+    {
+      println( "Factorial of " + i + ": = " + factorialRecurFuncTry(i) )
+    }
+
+    println ("--------------------");
+
+    printf ("----------End of %s----------\n" ,getClass.getName());
+    println ("--------------------");
+  }
+
+  //Recursion Functions
+  def factorialRecurFuncTry(n: BigInt): BigInt =
+  {  
+    if (n <= 1)
+      1;
+    else    
+      n * factorialRecurFuncTry(n - 1);
+  }
+
+}
