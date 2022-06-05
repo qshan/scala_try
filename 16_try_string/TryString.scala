8c57f105b3d624268d0ccd2b9b154234a2cc3e93
Frank Shan <frankqs0304@hotmail.com>
2022-06-05 23:53:43 +0800

    update the TryString case

--- 16_try_string/TryString.scala
+++ 16_try_string/TryString.scala
@@ -15,14 +15,29 @@ object TryString
     //printf ("--------------------\n");
 
     println ("---------- Try [String] here ----------");
-    var tempValue   : Int     = 0;
-    val tempInt01   : Int     = 2;
-    val tempInt02   : Int     = 3;
-    val tempStr     : String  = "Try String Data";
+    var tempIntValue  : Int     = 0;
+    var tempStrValue  : String  = "Init";
+    val tempInt01     : Int     = 2;
+    val tempInt02     : Int     = 3;
+    val tempStr       : String  = "Try String Data";
 
-    println( "greeting is " + greeting );
-    println( "tempStr is  " + tempStr );
+    println( "greeting      is " + greeting );
+    println( "tempStrValue  is " + tempStrValue );
+    tempStrValue  = tempStr;
+    println( "tempStrValue  is " + tempStrValue );
+    tempStrValue  += " Part added";
+    println( "tempStrValue  is " + tempStrValue );
 
+    println ("--------------------");
+
+    println ("---------- Try [StringBuilder] here ----------");
+    //pay attention to val here
+    val bufTryStringBuilder = new StringBuilder;
+    println( "bufTryStringBuilder is : " + bufTryStringBuilder.toString );
+    bufTryStringBuilder += 'a'
+    println( "bufTryStringBuilder is : " + bufTryStringBuilder.toString );
+    bufTryStringBuilder ++= "bcdef"
+    println( "bufTryStringBuilder is : " + bufTryStringBuilder.toString );
     println ("--------------------");
 
     println ("");
