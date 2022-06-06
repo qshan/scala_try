962bb443ebecffae5733b80a8b36487ee8ad34f3
Frank Shan <frankqs0304@hotmail.com>
2022-06-06 10:50:06 +0800

    add and udpate the Implicit class exampel

---
+++ 28_try_class03/TryClass03.scala
@@ -0,0 +1,44 @@
+#!scala
+
+// contents of Demo.scala file
+//import Run._
+//
+//object Demo
+//{
+//  def main(args: Array[String]) =
+//  {
+//    4 times println("hello")
+//  }
+//}
+
+import Run._
+
+object TryClass03
+{
+  def main(args: Array[String]): Unit =
+  {
+    println ("--------------------");
+    println("Hello, world! from " + getClass.getName());
+    println ("--------------------");
+    println ("");
+    //printf ("--------------------\n");
+
+    var tempIntValue  : Int     = 0;
+    var tempStrValue  : String  = "Init";
+    val tempInt01     : Int     = 2;
+    val tempInt02     : Int     = 3;
+    val tempStr       : String  = "Try String Data";
+
+    println ("---------- Try [Implicit Classes] here ----------");
+
+    4 times println("hello")
+
+    println ("--------------------");
+
+    println ();
+    printf ("----------End of %s----------\n" ,getClass.getName());
+    println ();
+    //println ("--------------------");
+  }
+
+}
